328358045c9125007511fa28a4b6dca9cda83e6f
Stef Claus <stef.claus@gmail.com>
2019-01-06 23:55:03 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,10 +29,12 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
-    FROM series JOIN characters ON series.author_id =  characters.author_id
-    WHERE characters.species = 'Human'
-    ORDER BY COUNT(characters.id) DESC
-    LIMIT 1;"
+    FROM series JOIN characters ON characters.series_id = series.id
+    WHERE characters.species = 'human'
+    GROUP BY series.title
+      ORDER BY COUNT(characters.species) DESC
+      LIMIT 1;"
+
 end
 
 def select_character_names_and_number_of_books_they_are_in
