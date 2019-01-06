962633bba8ff64d2809c4c302a510f545d9c48f9
Stef Claus <stef.claus@gmail.com>
2019-01-06 22:13:00 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+    FROM series JOIN subgenres ON series.subgenre_id = subgenres.id
+      JOIN authors ON series.author_id = authors.id;"
 end
 
 def select_series_title_with_most_human_characters
