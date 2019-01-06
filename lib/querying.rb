05c109aada1efb51ea4d5c6c05a68d521bef6e54
Stef Claus <stef.claus@gmail.com>
2019-01-06 22:01:58 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,24 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year
+    FROM books JOIN series ON series.id = books.series_id
+    WHERE series.id = 1
+    GROUP BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto
+    FROM characters
+    ORDER BY LENGTH(characters.motto) DESC
+    LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species)
+    FROM  characters
+    GROUP BY characters.species
+    ORDER BY COUNT(characters.species) DESC
+    LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
