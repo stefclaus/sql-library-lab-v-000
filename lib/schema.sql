6d4023735b9feebf7bc1e3ad8d67d0edd6042138
Stef Claus <stef.claus@gmail.com>
2019-01-06 21:05:32 +0000

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE series
+    (id INTEGER PRIMARY KEY,
+    title TEXT,
+    book_author TEXT,
+    book_sub_genre TEXT);
+
+
+CREATE TABLE sub_genre
+  (id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER);
+
+
+CREATE TABLE authors
+  (id INTEGER PRIMARY KEY,
+    amount INTEGER,
+    user_id INTEGER,
+    project_id INTEGER);
+
+CREATE TABLE books
+  (id INTEGER PRIMARY KEY,
+    amount INTEGER,
+    user_id INTEGER,
+    project_id INTEGER);
+
+CREATE TABLE character_books
+  (id INTEGER PRIMARY KEY,
+    amount INTEGER,
+    user_id INTEGER,
+    project_id INTEGER);
