71dce4118e4e2d085b83a20cdc2cda01385cd800
Stef Claus <stef.claus@gmail.com>
2019-01-07 00:00:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,7 +38,7 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.book_id)
-    FROM character_books JOIN books ON series.id = books.id
-      JOIN characters ON series.author_id = characters.author_id
+    FROM character_books JOIN characters ON series.id = books.id
+      JOIN books ON series.author_id = characters.author_id
     GROUP BY characters.name;"
 end
